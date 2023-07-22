a3042b622c14d1c4e679e4d1e899b2ecf6e197e2
ThomasChin <140190333+ThomasChin@users.noreply.github.com>
2023-07-22 22:47:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-// Function to get and display the leaderboard
-function showLeaderboard() {
-  let scores = JSON.parse(localStorage.getItem('scores')) || [];
-  let leaderboard = scores
-    .sort((a, b) => b.score - a.score)
-    .map((entry, index) => `<p>${index + 1}. ${entry.name}: ${entry.score}</p>`)
-    .join('');
-  document.getElementById('score-container').innerHTML = leaderboard;
+const firebaseConfig = {
+  apiKey: "AIzaSyDz0D3gOMDNIO5ZK_yleQk1-p7VxgkUnTg",
+  authDomain: "thomas-chin.firebaseapp.com",
+  projectId: "thomas-chin",
+  storageBucket: "thomas-chin.appspot.com",
+  messagingSenderId: "520663111964",
+  appId: "1:520663111964:web:c2a1d757b761c08774c2d9",
+  measurementId: "G-KWDSQ2ZF68"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let score = 0;
+const leaderboardRef = firebase.database().ref("leaderboard");
+
+function updateScore() {
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = score.toString();
 }
 
-// Function to start the game and record the score
-function playGame() {
-  const playerName = prompt('Enter your name:');
-  if (!playerName) return;
+function updateLeaderboard() {
+    leaderboardRef.orderByChild("score").limitToLast(10).on("value", (snapshot) => {
+        const leaderboardElement = document.getElementById("leaderboard");
+        leaderboardElement.innerHTML = "";
 
-  // For simplicity, let's assume the game generates a random score between 0 and 100
-  const score = Math.floor(Math.random() * 101);
-
-  let scores = JSON.parse(localStorage.getItem('scores')) || [];
-  scores.push({ name: playerName, score: score });
-  localStorage.setItem('scores', JSON.stringify(scores));
-
-  showLeaderboard();
+        snapshot.forEach((childSnapshot) => {
+            const entry = childSnapshot.val();
+            const listItem = document.createElement("li");
+            listItem.textContent = `${entry.name} - ${entry.score}`;
+            leaderboardElement.appendChild(listItem);
+        });
+    });
 }
 
-// Attach event listener to the "Play Game" button
-document.getElementById('play-btn').addEventListener('click', playGame);
+function handleClick() {
+    score += 1;
+    updateScore();
 
-// Show the leaderboard when the page loads
-showLeaderboard();
+    const playerName = prompt("Congratulations! You achieved a score of " + score + ". Please enter your name:");
+
+    // Push the new score and name to the leaderboard
+    leaderboardRef.push({ name: playerName, score: score });
+}
+
+const clickButton = document.getElementById("clickButton");
+clickButton.addEventListener("click", handleClick);
+
+// Call the updateLeaderboard function to initialize the leaderboard
+updateLeaderboard();
